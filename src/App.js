648fc9112c5a3fb8c6e20df44aa7cb069a3ff602
Mikko Metso <mikkosanterimetso@gmail.com>
2019-03-03 15:42:47 +0200

    added hardcoded state data to project for testing events

--- src/App.js
+++ src/App.js
@@ -4,12 +4,29 @@ import WeatherDisplay from './WeatherDisplay/WeatherDisplay';
 import WeatherSearch from './WeatherSearch/WeatherSearch';
 
 class App extends Component {
+  state = {
+    forecasts : [
+      {      
+        city: 'Malaga', 
+        temp: '28.4',
+        desc: 'Sunny as always'
+      },
+      {
+        city: 'Benalmadena', 
+        temp: '26.4',
+        desc: 'Nice summer weather'
+      }
+    ]
+  };
+  
+
   render() {
     return (
       <div className="App">
         <h1>My Weather App MVP</h1>
         <WeatherSearch />
-        <WeatherDisplay city="Malaga" temp="26.4" desc="Sunny as always"/>  
+        <WeatherDisplay city={this.state.forecasts[0].city} temp={this.state.forecasts[0].temp} desc={this.state.forecasts[0].desc} />
+        <WeatherDisplay city={this.state.forecasts[1].city} temp={this.state.forecasts[1].temp} desc={this.state.forecasts[1].desc} />
       </div>
     );
   }
