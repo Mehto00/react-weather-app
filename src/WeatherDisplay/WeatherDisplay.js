ecf4e2cb58279c1c48afe08e078360dcad4c1b1f
Mikko Metso <mikko.metso@frantic.com>
2019-03-05 20:36:02 +0200

    fixed small merge-conflict in weatherDisplay.js

--- src/WeatherDisplay/WeatherDisplay.js
+++ src/WeatherDisplay/WeatherDisplay.js
@@ -3,10 +3,12 @@ import './WeatherDisplay.css';
 
 const weatherDisplay = (props) => {
     return (
+
     <div className="weatherDisplay">        
-        <h2>{props.city}</h2>
+        <h2 className="weatherDisplay__header">{props.city}</h2>
         <span className="weatherDisplay__temp">
             {props.temp}<span className="weatherDisplay__cUnit">&#8451;</span>
+
         </span>
         <br/>
         <span className="weatherDisplay__desc">
