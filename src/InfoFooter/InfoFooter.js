390d9b40d222fcb594bfb2d85a65c52167896d67
Mikko Metso <mikko.metso@frantic.com>
2019-03-08 08:12:46 +0200

    Tested styling to have a sticky footer on the bottom. Still noot quite there yet. Also cleaned up some copypastes and added the correct links to footer

--- src/InfoFooter/InfoFooter.js
+++ src/InfoFooter/InfoFooter.js
@@ -6,8 +6,8 @@ const infoFooter = () => {
         <div className="infoFooter">
             <ul className="infoFooter__list">
                 <li className="infoFooter__list__items"><a href="https://www.github.com/Mehto00">Mehto00 did this</a></li>
-                <li className="infoFooter__list__items"><a href="https://www.github.com/Mehto00">Repo available here</a></li>
-                <li className="infoFooter__list__items"><a href="https://www.github.com/Mehto00">LinkedIn stuff here</a></li>
+                <li className="infoFooter__list__items"><a href="https://github.com/Mehto00/react-weather-app">Repo here</a></li>
+                <li className="infoFooter__list__items"><a href="https://www.linkedin.com/in/mikkometso/">LinkedIn stuff here</a></li>
             </ul>
         </div>
     )
